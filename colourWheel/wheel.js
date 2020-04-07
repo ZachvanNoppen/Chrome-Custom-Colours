7b495bc1ba83b8bf0fb192c9590b3cb36b528610
Zachary van Noppen <dzacharyvannoppen@gmail.com>
2020-04-07 18:01:04 -0400

    Made Colour Wheel

--- colourWheel/wheel.js
+++ colourWheel/wheel.js
@@ -1,9 +1,28 @@
 let circle = {
+  RADIUS: 70,
+  DRAG: false,
+  DIMENSIONS: {
+    width: "auto", //"100px"
+    height: "auto"
+  },
+  CUR_COLOUR:{
+    r:0,
+    g:0,
+    b:0,
+    a:100,
+  },
   init: function(){
     let canvas = document.getElementById('canvas');
+    this.setupCanvas(canvas);
     let ctx = canvas.getContext("2d");
-    this.drawCircle(ctx);
+    //Drawing circle on screen
+    this.drawCircle(ctx, canvas,0,0);
+    //Setting up even handlers
     this.events(ctx);
+  },
+  setupCanvas: function(canvas){
+    canvas.style.width = this.DIMENSIONS.width;
+    canvas.style.height = this.DIMENSIONS.height;
   },
   // All event handlers
   events: function(ctx){
@@ -11,24 +30,32 @@ let circle = {
     let self = this;
     canvas.addEventListener("mousedown", function(e)
     {
-        self.getMousePosition(canvas, ctx, e);
+        self.DRAG = true;
     });
-    canvas.addEventListener("drag", function(e)
+    canvas.addEventListener("mousemove", function(e)
     {
+      if(self.DRAG){
         self.getMousePosition(canvas, ctx, e);
+        self.setColour();
+      }
+    });
+    canvas.addEventListener("mouseup", function(e)
+    {
+        self.DRAG = false;
     });
   },
-  drawCircle: function(ctx){
-    let radius = 50;
-    let image = ctx.createImageData(2*radius, 2*radius);
+  drawCircle: function(ctx, canvas, testX,testY){
+    let image = ctx.createImageData(2*this.RADIUS, 2*this.RADIUS);
     let data = image.data;
+    let offsetX = (canvas.width - this.RADIUS*2) / 2;
+    let offsetY = (canvas.height - this.RADIUS*2) / 2;
 
-    for (let x = -radius; x < radius; x++) {
-      for (let y = -radius; y < radius; y++) {
+    for (let x = -this.RADIUS; x < this.RADIUS; x++) {
+      for (let y = -this.RADIUS; y < this.RADIUS; y++) {
 
         let [r, phi] = this.xy2polar(x, y);
 
-        if (r > radius) {
+        if (r > this.RADIUS) {
           // skip all (x,y) coordinates that are outside of the circle
           continue;
         }
@@ -36,14 +63,14 @@ let circle = {
         let deg = this.rad2deg(phi);
 
         // Figure out the starting index of this pixel in the image data array.
-        let rowLength = 2*radius;
-        let adjustedX = x + radius; // convert x from [-50, 50] to [0, 100] (the coordinates of the image data array)
-        let adjustedY = y + radius; // convert y from [-50, 50] to [0, 100] (the coordinates of the image data array)
+        let rowLength = 2*this.RADIUS;
+        let adjustedX = x + this.RADIUS; // convert x from [-50, 50] to [0, 100] (the coordinates of the image data array)
+        let adjustedY = y + this.RADIUS; // convert y from [-50, 50] to [0, 100] (the coordinates of the image data array)
         let pixelWidth = 4; // each pixel requires 4 slots in the data array
         let index = (adjustedX + (adjustedY * rowLength)) * pixelWidth;
 
         let hue = deg;
-        let saturation = r / radius;
+        let saturation = r / this.RADIUS;
         let value = 1.0;
 
         let [red, green, blue] = this.hsv2rgb(hue, saturation, value);
@@ -53,12 +80,33 @@ let circle = {
         data[index+1] = green;
         data[index+2] = blue;
         data[index+3] = alpha;
+
+        //Setting the current colour
+        if(testX == adjustedX+offsetX && testY == adjustedY+offsetY){
+            this.CUR_COLOUR.r = red;
+            this.CUR_COLOUR.g = green;
+            this.CUR_COLOUR.b = blue;
+            this.CUR_COLOUR.a = alpha;
+        }
       }
     }
-
-    ctx.putImageData(image, 0, 0);
+    ctx.putImageData(image, offsetX, offsetY);
   },
+  setColour: function(colour = null){
+    //If colour is not null it expects an object in the format {r,g,b,a}
+    if(colour == null){
+      //if no colour is specified, use the one in the colour picker
+      document.getElementById("display").style.background = "rgba("+this.CUR_COLOUR.r+","+this.CUR_COLOUR.g+","+this.CUR_COLOUR.b+","+this.CUR_COLOUR.a+")";
+      //Debugging
+      //console.log("rgba("+this.CUR_COLOUR.r+","+this.CUR_COLOUR.g+","+this.CUR_COLOUR.b+","+this.CUR_COLOUR.a+")");
+    }else{
+      //the colour is specified
+      document.getElementById("display").style.background = "rgba("+colour.r+","+colour.g+","+colour.b+","+colour.a+")";
 
+    }
+
+
+  },
   xy2polar: function(x,y){
     let r = Math.sqrt(x*x + y*y);
     let phi = Math.atan2(y, x);
@@ -75,16 +123,18 @@ let circle = {
     let rect = canvas.getBoundingClientRect();
     let x = event.clientX - rect.left;
     let y = event.clientY - rect.top;
-    console.log("Coordinate x: " + x,"Coordinate y: " + y);
 
-    ctx.clearRect(0,0,rect.left, rect.top);
-    this.drawCircle(ctx);
-
+    ctx.clearRect(0,0,canvas.width, canvas.height);
+    this.drawCircle(ctx, canvas,x,y);
 
     ctx.beginPath();
     ctx.arc(x, y, 2, 0, 2 * Math.PI);
     ctx.stroke();
+
+    //for debugging
+    return [x,y];
   },
+
   // hue in range [0, 360]
   // saturation, value in range [0,1]
   // return [r,g,b] each in range [0,255]
